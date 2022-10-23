2a77890b680891e8aef08e8a969bbffe820add6f
tuiqpan <ionlac@alum.us.es>
2022-10-23 18:14:17 +0200

    First

--- src/module1.py
+++ src/module1.py
@@ -6,7 +6,7 @@ from parsers import *
 #Let BD as an abbreviation of "Bank Data"
 
 
-BD = namedtuple("BD", "First_name,Last_name,Email,Address,Phone_Number,Date_Of_Birth,Balance,Credit_Card,Has_More_Cards,Card_Type")
+BD = namedtuple("BD", "First_name,Last_name,Email,Address,Phone_Number,Date_Of_Birth,Balance,Credit_Card,Mortage,Card_Type")
 
 def lector(file):
     with open(file, "r", encoding='utf-8')as f:
@@ -14,8 +14,8 @@ def lector(file):
         next(lector)
         register=[]
 
-        for First_name,Last_name,Email,Address,Phone_Number,Date_Of_Birth,Balance,Credit_Card,Has_More_Cards,Card_Type in lector:
-            res = BD(str(First_name), str(Last_name), str(Email), str(Address), str(Phone_Number), datetime_parsing(Date_Of_Birth), balance_parsing(Balance), int(Credit_Card), bool(Has_More_Cards), str(Card_Type))
+        for First_name,Last_name,Email,Address,Phone_Number,Date_Of_Birth,Balance,Credit_Card,Mortage,Card_Type in lector:
+            res = BD(str(First_name), str(Last_name), str(Email), str(Address), str(Phone_Number), datetime_parsing(Date_Of_Birth), balance_parsing(Balance), int(Credit_Card), bool(Mortage), str(Card_Type))
 
             register.append(res)
 
